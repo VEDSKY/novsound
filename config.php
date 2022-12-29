2ab462025119ab65cfc35280637f8c310a618a94
VEDSKY <89seredina@gmail.com>
2022-12-30 00:16:28 +0300

    Base application config included

--- config.php
+++ config.php
@@ -1,3 +1,24 @@
 <?php
 
-const ENGINE ='vSpaceLite';
+$APPLICATION_CONSTANTS = 
+[
+    'APP_PATH'      => dirname(__FILE__) . '/',  // Absolute path to application location.
+    'ENGINE'        => 'vSpaceLite',             // Name of the used engine. must be equals engine folder name 
+    'DEBUG_MODE'    => false,
+    'DB_CONFIG'     => 
+    [
+        'DB_HOST'   => 'localhost',
+        'DB_PORT'   => '',
+        'DB_NAME'   => '',
+        'DB_USER'   => '',
+        'DB_PASS'   => ''
+    ],
+    'TESTING_MODE'  => true
+];
+
+
+
+if (!defined('APC')){
+    define('APC',$APPLICATION_CONSTANTS);
+}
+
